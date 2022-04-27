af746a5eb3e48b011df8fac9141a8bdac82c1e3e
Lihan1102 <a223828@gmail.com>
2022-04-27 20:24:35 +0800

    add new file

--- #.py
+++ #.py
@@ -9,6 +9,16 @@ with open('original.txt', 'r') as f:
 			print(len(data))
 print('總共有',len(data),'筆資料')
 
+sum_len = 0
 for d in data:
 	sum_len = sum_len + len(d)
-	print('平均留言長度',sum_len/len(data))
+print('平均留言長度',sum_len/len(data))
+
+
+#篩選留言<100
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有',len(new),'留言長度小於100')
+print(new[0])
